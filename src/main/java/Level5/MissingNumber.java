cc3a36da379f85217f835c9643fabbcfbb17a630
Saskia Opdam <saskia.opdam@duo.nl>
2024-05-12 12:06:33 +0200

    Random tests fail: handle string remainder?

--- src/main/java/Level5/MissingNumber.java
+++ src/main/java/Level5/MissingNumber.java
@@ -1,10 +1,11 @@
+// Simple number sequence - https://www.codewars.com/kata/5a28cf591f7f7019a80000de
 package Level5;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class MissingNumber {
-    public static int missing(String s){
+    public static int missing(String s) {
         String stringToCheck = s;
         int digits = 1;
         int start = Integer.parseInt(stringToCheck.substring(0, digits));
@@ -15,23 +16,9 @@ class MissingNumber {
 
         while (expectedDigits <= stringToCheck.length()) {
             int actual = Integer.parseInt(stringToCheck.substring(0, expectedDigits));
-            String actualString = stringToCheck.substring(0, expectedDigits);
 
-            /// LAATSTE STUKJE STRING < EXPECTED STRING: repeat loop : HOE???
-            if (expectedDigits > stringToCheck.length()) {
-//                System.out.println("actual smaller than expected");
-                missingNumbers.clear();
-                stringToCheck = s;
-                digits++;
-                start = Integer.parseInt(stringToCheck.substring(0, digits));
-                expected = start + 1;
-                expectedDigits = String.valueOf(expected).length();
-                stringToCheck = s.replaceFirst(String.valueOf(start), "");
-                continue;
-            }
-
-            if (actual < expected) {
-//                System.out.println("actual smaller than expected");
+            // How also repeat loop when remaining string to check is shorter than expected digits?
+            if (actual < expected || actual > expected + 1) {
                 missingNumbers.clear();
                 stringToCheck = s;
                 digits++;
@@ -43,31 +30,35 @@ class MissingNumber {
             }
 
             if (actual == expected) {
-//                System.out.println("actual equals expected");
                 expected++;
                 expectedDigits = String.valueOf(expected).length();
-                stringToCheck = stringToCheck.replaceFirst(actualString, "");
+                stringToCheck = stringToCheck.replaceFirst(String.valueOf(actual), "");
             }
 
-            if (actual > expected) {
+            if (actual == expected + 1) {
                 missingNumbers.add(expected);
                 expected++;
             }
+
+            System.out.println("start: " + start);
+            System.out.println("expected (start + 1): " + expected);
+            System.out.println("remaining: " + stringToCheck);
+            System.out.println("length of expected string: " + expectedDigits);
+            System.out.println("length of remaining string: " + stringToCheck.length());
         }
 
-        System.out.println("missing numbers: " + missingNumbers);
-
         if (missingNumbers.isEmpty()) {
-            System.out.println("none missing: -1");
+            System.out.println("none missing");
             return -1;
         }
         else if (missingNumbers.size() > 1) {
-            System.out.println("more than one missing: -1");
+            System.out.println("more than one missing");
             return -1;
         }
         else {
-            System.out.println("exactly one missing: " + missingNumbers.get(0));
+            System.out.println("exactly one missing");
             return missingNumbers.get(0);
         }
+
     }
 }
--- src/test/java/Level5/MissingNumberTest.java
+++ src/test/java/Level5/MissingNumberTest.java
@@ -8,13 +8,18 @@ class MissingNumberTest {
     @Test
     public void basicTests(){
         assertEquals(4,MissingNumber.missing("123567"));
-//        assertEquals(92,MissingNumber.missing("899091939495")); // *** faalt, ipv [92] enorme lijst
+        assertEquals(92,MissingNumber.missing("899091939495"));
         assertEquals(100,MissingNumber.missing("9899101102"));
         assertEquals(-1,MissingNumber.missing("599600601602"));
-        assertEquals(-1,MissingNumber.missing("8990919395")); // ** slaagt, maar ipv [92, 94] enorme lijst
+        assertEquals(-1,MissingNumber.missing("8990919395"));
         assertEquals(1002,MissingNumber.missing("998999100010011003"));
         assertEquals(10000,MissingNumber.missing("99991000110002"));
         assertEquals(-1,MissingNumber.missing("979899100101102"));
         assertEquals(900003,MissingNumber.missing("900001900002900004900005900006"));
+    }
+    @Test
+    public void randomTests(){
+        assertEquals(-1,MissingNumber.missing("9993999499"));
+        assertEquals(-1,MissingNumber.missing("9929939949"));
     }
 }
